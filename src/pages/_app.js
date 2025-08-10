83e12ca2002d0c077d8e9cc79a7643cfc26f0637
Hu man <romaincontant.80@gmail.com>
2025-08-10 20:36:36 +0200

    fix: use relative path for global stylesheet

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import { AppProvider } from '@/context/AppContext';
 import { contents } from '@/data/contents';
-import '@/styles.css';
+import '../styles.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 export default function MyApp({ Component, pageProps }) {
