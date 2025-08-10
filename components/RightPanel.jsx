4d1952ab51abe38547ef888f40bbd8c1ee469d20
Hu man <romaincontant.80@gmail.com>
2025-08-10 21:46:15 +0200

    fix: ensure RightPanel is valid React component

--- components/RightPanel.jsx
+++ components/RightPanel.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { emotions, emotionKeys } from '../utils/constants.js';
 import styles from './RightPanel.module.css';
 
