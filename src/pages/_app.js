8f18c4177f87450b47aadf3c9e764485fed4b463
Hu man <romaincontant.80@gmail.com>
2025-08-10 19:26:03 +0200

    fix: add js _app entry

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,9 @@
-import type { AppProps } from 'next/app';
 import { AppProvider } from '@/context/AppContext';
 import { contents } from '@/data/contents';
-import '../styles.css';
+import '@/styles.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <AppProvider initialContents={contents}>
       <Component {...pageProps} />
