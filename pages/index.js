64f464ccb3364a4dd3ddfd661cc9eda6f153423d
Hu man <romaincontant.80@gmail.com>
2025-08-10 18:09:58 +0200

    Integrate self-contained Mapbox globe

--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,5 @@
-import { useState, useRef, useEffect } from 'react';
 import HuMapboxGlobe from '../components/HuMapboxGlobe';
-import TopBar from '../components/TopBar';
-import LeftPanel from '../components/LeftPanel';
-import RightPanel from '../components/RightPanel';
-import { sampleData } from '../data/sampleData.js';
 
 export default function Home() {
-  const [view, setView] = useState('video');
-  const [selectedEmotions, setSelectedEmotions] = useState([]);
-  const [selectedCountry, setSelectedCountry] = useState(null);
-  const [bias, setBias] = useState(true);
-  const [mapStyle, setMapStyle] = useState('light');
-  const [theme, setTheme] = useState('light');
-  const mapRef = useRef(null);
-
-  useEffect(() => {
-    document.body.classList.remove('theme-light', 'theme-dark');
-    document.body.classList.add(`theme-${theme}`);
-  }, [theme]);
-
-  return (
-    <div className="app">
-      <TopBar
-        view={view}
-        setView={setView}
-        mapStyle={mapStyle}
-        setMapStyle={setMapStyle}
-        theme={theme}
-        setTheme={setTheme}
-      />
-      <LeftPanel
-        selectedCountry={selectedCountry}
-        setSelectedCountry={setSelectedCountry}
-        bias={bias}
-        setBias={setBias}
-      />
-      <HuMapboxGlobe
-        ref={mapRef}
-        data={sampleData}
-        view={view}
-        emotions={selectedEmotions}
-        country={selectedCountry}
-        mapStyle={mapStyle}
-      />
-      <RightPanel
-        data={sampleData}
-        selectedEmotions={selectedEmotions}
-        setSelectedEmotions={setSelectedEmotions}
-        view={view}
-      />
-      <button className="myPosition" onClick={() => mapRef.current?.centerOnUser()}>
-        Ma position
-      </button>
-    </div>
-  );
+  return <HuMapboxGlobe />;
 }
